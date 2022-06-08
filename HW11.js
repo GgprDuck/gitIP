aa5067017ffd0eb383a00d1a49fe79379f91eeb1
GgprDuck <sashacazachcov91@gmail.com>
2022-06-09 01:09:29 +0300

    new Task

---
+++ HW11.js
@@ -0,0 +1,25 @@
+const { networkInterfaces } = require('os');
+const http = require('http');
+const nets = networkInterfaces();
+
+const getIp = function (option) {
+    if (option === 'true') {
+        for (const name of Object.keys(nets)) {
+            for (const net of nets[name]) {
+                const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+                if (net.family === familyV4Value && !net.internal) {
+                    console.log(net.address);
+                }
+            }
+        }
+    }
+    else{
+     http.get({ 'host': 'api.ipify.org', 'port': 80, 'path': '/' }, function (resp) {
+        resp.on('data', function (ip) {
+            console.log("My public IP address is: " + ip);
+        });
+    });   
+    }
+}
+
+module.exports = getIp;
